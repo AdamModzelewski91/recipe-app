62d425b2239a06bed7da0db45e79a6a7ae24c33a
adam <amodzelewski91@gmail.com>
2024-03-24 10:25:12 +0100

    feat(recipes-table): add base html concept

--- src/app/reciepes/my-recipe-list/my-recipe-list.component.ts
+++ src/app/reciepes/my-recipe-list/my-recipe-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed } from '@angular/core';
 import { MyRecipesService } from '../services/my-recipes.service';
 import { JsonPipe, NgFor } from '@angular/common';
+import { RecipesTableComponent } from '../components/recipes-table/recipes-table.component';
 
 @Component({
   selector: 'app-my-recipe-list',
   standalone: true,
-  imports: [NgFor, JsonPipe],
+  imports: [NgFor, JsonPipe, RecipesTableComponent],
   templateUrl: './my-recipe-list.component.html',
   styleUrl: './my-recipe-list.component.scss'
 })
--- src/app/reciepes/services/my-recipes.service.ts
+++ src/app/reciepes/services/my-recipes.service.ts
@@ -6,7 +6,53 @@ import { RecipeWithPhotos } from '../models/recipe.type';
 })
 export class MyRecipesService {
 
-  myRecipes = signal<RecipeWithPhotos[]>([])
+  myRecipes = signal<RecipeWithPhotos[]>([
+    {
+      name: "Apple pie",
+      dish: "cake",
+      difficult: "moderate",
+      prepTime: "20",
+      cookTime: "180",
+      serves: "12",
+      nutritions: {
+        calories: "300",
+        fat: "25",
+        carbohydrate: "18",
+        protein: "6"
+      },
+      photos: []
+    },
+    {
+      name: "Apple pie",
+      dish: "cake",
+      difficult: "moderate",
+      prepTime: "20",
+      cookTime: "180",
+      serves: "12",
+      nutritions: {
+        calories: "300",
+        fat: "25",
+        carbohydrate: "18",
+        protein: "6"
+      },
+      photos: []
+    },
+    {
+      name: "Apple pie",
+      dish: "cake",
+      difficult: "moderate",
+      prepTime: "20",
+      cookTime: "180",
+      serves: "12",
+      nutritions: {
+        calories: "300",
+        fat: "25",
+        carbohydrate: "18",
+        protein: "6"
+      },
+      photos: []
+    },
+  ])
 
   constructor() { }
 
